937f9fd234f30bf43aa7ad68cac322c068c1f968
isuru <isurucuma@gmail.com>
2023-06-25 10:20:58 +0530

    social media token handle added

--- front-end/components/page_items/admin/KeysPageContainer.tsx
+++ front-end/components/page_items/admin/KeysPageContainer.tsx
@@ -4,9 +4,12 @@ import React, { useState, useEffect } from "react";
 
 type Props = {};
 
-function KeysPageContainer({ }: Props) {
+function KeysPageContainer({}: Props) {
   const [openApiKey, setOpenApiKey] = useState("");
-  const handleSubmit = () => {
+  const [fbToken, setFbToken] = useState("");
+  const [waToken, setWaToken] = useState("");
+
+  const handleAPIKeySubmit = () => {
     fetch("http://localhost:3002/updateKey", {
       method: "POST",
       headers: {
@@ -17,8 +20,21 @@ function KeysPageContainer({ }: Props) {
       .then((response) => response.json())
       .then((data) => {
         console.log("Success:", data);
-      }
-      );
+      });
+  };
+
+  const handleSocialMediaTokenSubmit = () => {
+    fetch("http://localhost:3002/updateSocialKeys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ fbToken: fbToken, waToken: waToken }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      });
   };
 
   useEffect(() => {
@@ -30,25 +46,60 @@ function KeysPageContainer({ }: Props) {
   }, []);
 
   return (
-    <div className="mt-40 flex flex-row justify-center gap-20 items-center">
-      <form>
-        <label
-          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-          htmlFor="name"
-        >
-          OpenAI API Key
-        </label>
-        <input
-          className="flex h-10 w-96 rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-          type="text"
-          placeholder="Enter your OpenAI API key"
-          id="name"
-          value={openApiKey}
-          onChange={(e) => setOpenApiKey(e.target.value)}
-        ></input>
-        <p className="mt-1 text-xs text-gray-500">*This field is required</p>
-      </form>
-      <Button name="Submit" className="my-8" onClick={handleSubmit} />
+    <div className="mt-32">
+      <div className="mt-8 flex flex-row justify-center gap-20 items-center">
+        <form>
+          <label
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            htmlFor="name"
+          >
+            OpenAI API Key
+          </label>
+          <input
+            className="flex h-10 w-96 rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            type="text"
+            placeholder="Enter your OpenAI API key"
+            id="name"
+            value={openApiKey}
+            onChange={(e) => setOpenApiKey(e.target.value)}
+          ></input>
+          <p className="mt-1 text-xs text-gray-500">*This field is required</p>
+        </form>
+        <Button name="Submit" className="my-8" onClick={handleAPIKeySubmit} />
+      </div>
+      <div className="mt-8 flex flex-row justify-center gap-20 items-center">
+        <div>
+          <h4 className="text-sm font-medium mb-2 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Social Media Tokens
+          </h4>
+          <form className="mb-2">
+            <input
+              className="flex h-10 w-96 rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+              type="text"
+              placeholder="Enter your WhatsApp token"
+              id="name"
+              value={waToken}
+              onChange={(e) => setWaToken(e.target.value)}
+            ></input>
+          </form>
+          <form>
+            <input
+              className="flex h-10 w-96 rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+              type="text"
+              placeholder="Enter your Facebook token"
+              id="name"
+              value={fbToken}
+              onChange={(e) => setFbToken(e.target.value)}
+            ></input>
+          </form>
+        </div>
+
+        <Button
+          name="Submit"
+          className="my-8"
+          onClick={handleSocialMediaTokenSubmit}
+        />
+      </div>
     </div>
   );
 }
